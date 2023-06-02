46b82fe03145ea4a5f8a3efbb3e4a2ec718b20d9
Danveer Sepersad <sepersadk@gmail.com>
2023-06-02 09:31:50 +0400

    srver not working

---
+++ src/components/app.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable semi */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+/* eslint-disable object-shorthand */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+    this.search = this.search.bind(this);
+    this.selectGif = this.selectGif.bind(this);
+  }
+
+  search(query) {
+    const giphEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${query}&limit=10`;
+    fetch(giphEndpoint).then(response => response.json()).then((data) => {
+      const gifs = data.data.map(giph => giph.id);
+      this.setState({
+        gifs: gifs,
+      })
+    // eslint-disable-next-line semi
+    })
+  }
+
+  selectGif(id) {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/gif.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint jsx-a11y/alt-text: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render() {
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} className="gif" onClick={this.handleClick} />
+    );
+  }
+}
+
+export default Gif;
